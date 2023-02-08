b7afaee70f553adc9d3490e17a1dd34d62566376
sven-cork <linslus81@gmail.com>
2023-02-08 15:46:31 +0000

    Add game board variable and select_player and user_input functions

--- run.py
+++ run.py
@@ -1,5 +1,24 @@
+
+
 game_board = ["-","-","-",
               "-","-","-"
               "-","-","-"]
 
 print(game_board)
+
+
+def select_player():
+    player = ""
+    while player != "X" or player != "O": 
+        player = input("Player select team 'X' or 'O'")
+
+
+def user_input(game_board):
+    """
+    User selects slot to place X or O. Checks if slot already taken and if so 
+    prompts user to select a new slow.
+    """
+
+    user_input = int(input("Select a free slot (number 1-9) on the game board to achive three in a row: "))
+    if user_input >= 1 and user_input <= 9 and game_board[user_input] != "-":
+        game_board[user_input] = 
