0cfe49a90173544fa2d0a8eb95611d7d83b6456c
sven-cork <linslus81@gmail.com>
2023-02-09 13:00:16 +0000

    add content to check_winner function

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 
 
-game_board = ["-","-","-",
-              "-","-","-"
-              "-","-","-"]
+game_board = [["-","-","-"],
+              ["-","-","-"],
+              ["-","-","-"]]
 
 print(game_board)
 
@@ -18,7 +18,7 @@ print(game_board)
 #computer turn
 #winner
 
-
+#select_player
 def select_player():
 
     """
@@ -35,7 +35,7 @@ def select_player():
     return player
     
 
-
+#select slot on board and update board
 def user_input(game_board):
     """
     User selects slot to place X or O. Checks if slot already taken and if so 
@@ -45,5 +45,16 @@ def user_input(game_board):
     user_input = int(input("Select a free slot (number 1-9) on the game board to achive three in a row: "))
     if user_input >= 1 and user_input <= 9 and game_board[user_input] != "-":
         game_board[user_input] = player
+
+#winner?
+
+def check_winner(game_board):
+    #check horizontal win condition
+    if horizontal_win = game_board[0,0] == game_board[0,1] == game_board[0,2] and game_board[0,0] != "-"
+    elif:
+        game_board[1,0] == game_board[1,1] == game_board[1,2] and game_board[1,0] != "-"
+    elif:  
+        game_board[2,0] == game_board[2,1] == game_board[2,2] and game_board[2,0] != "-"  
+
 
 select_player()
