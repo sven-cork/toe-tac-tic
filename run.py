ca58141fcfba5ce1a787f6a7dcd73e56b00edb4a
sven-cork <linslus81@gmail.com>
2023-02-11 11:18:36 +0000

    Add function display_board()

--- run.py
+++ run.py
@@ -1,8 +1,16 @@
-game_board = [1, 2, 3,
-              4, 5, 6,
-              7, 8, 9]
+game_board = ["-", "-", "-",
+              "-", "-", "-",
+              "-", "-", "-"]
+              
+              
 
 print(game_board)
+
+player = ""
+while True: 
+        player = input("Player select team 'X' or 'O': ").upper()
+        if player == "X" or player == "O":
+            break
 
 
 #global variables:
@@ -10,6 +18,7 @@ print(game_board)
 #player?
 
 #functions:
+#display board
 #select_player
 #select slot on board and update board
 #winner
@@ -17,11 +26,12 @@ print(game_board)
 #winner
 
 #select_player
-def select_player():
+"""
+    def select_player():
 
-    """
-    Let player select team "X" or "O". Checks if either of the valid teams (characters) have been selected and corrects for lower case entered character."
-    """
+    
+    #Let player select team "X" or "O". Checks if either of the valid teams (characters) have been selected and corrects for lower case entered character."
+    
 
     player = ""
 
@@ -31,7 +41,16 @@ def select_player():
             break
     print(player)
     return player
-    
+"""
+
+def display_board(game_board):
+    print("+---+---+---+")
+    print("| " + game_board[0] + " | " + game_board[1] + " | " + game_board[2] + " | ")
+    print("+---+---+---+")
+    print("| " + game_board[3] + " | " + game_board[4] + " | " + game_board[5] + " | ")
+    print("+---+---+---+")
+    print("| " + game_board[6] + " | " + game_board[7] + " | " + game_board[8] + " | ")
+    print("+---+---+---+")
 
 #select slot on board and update board
 def user_input(game_board):
@@ -41,35 +60,40 @@ def user_input(game_board):
     """
 
     user_input = int(input("Select a free slot (number 1-9) on the game board to achive three in a row: "))
-    if user_input >= 1 and user_input <= 9 and game_board[user_input] != "-":
-        game_board[user_input] = player
+    if user_input >= 1 and user_input <= 9 and (game_board[user_input] != "X" or game_board[user_input] != "O"):
+        game_board[user_input - 1] = player
+    else:
+        print("Try again")
 
 #winner?
 
 def check_winner(game_board):
     #check horizontal win condition
     if game_board[0] == game_board[1] == game_board[2] and game_board[0] != "-":
-        return game_board[0]
+        print(game_board[0])
     elif game_board[3] == game_board[4] == game_board[5] and game_board[3] != "-":
-        return game_board[2]
+        print(game_board[2])
     elif game_board[6] == game_board[7] == game_board[8] and game_board[6] != "-":  
-        return game_board[6]
+        print(game_board[6])
 
     #check vertical win condition
     if game_board[0] == game_board[2] == game_board[6] and game_board[0] != "-":
-        return game_board[0]
+        prit(game_board[0])
     elif game_board[1] == game_board[3] == game_board[6] and game_board[1] != "-":
-        return game_board[1]
+        print(game_board[1])
     elif game_board[2] == game_board[5] == game_board[8] and game_board[2] != "-":  
-        return game_board[2]
+        print(game_board[2])
 
     #check diagnoal win condition
     if game_board[0] == game_board[4] == game_board[7] and game_board[0] != "-":
-        return game_board[0]
+        print(game_board[0])
     elif game_board[2] == game_board[4] == game_board[6] and game_board[2] != "-":
-        return game_board[2]
+        print(game_board[2])
 
 
-select_player()
+#select_player()
+display_board(game_board)
 user_input(game_board)
+display_board(game_board)
 check_winner(game_board)
+print("Computer turn")
